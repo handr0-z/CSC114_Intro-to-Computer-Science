ba8b1641edb330ba6acd11aff058a1870236fbd8
handr0-z <AlejandroZurita@live.com>
2026-04-20 19:58:25 -0400

    Add files via upload

---
+++ CSC114-Module1-Lab1/CSC114-Module1-Lab1/CSC114-Module1-Lab1.cpp
@@ -0,0 +1,31 @@
+//-------------------------------------------------------------------------
+//  College:        Delaware Techincal Community College
+//  Course:         CSC 114: Intro to Computer Science
+//  Instructor:     Instructor Mike (Thompson)
+//  Author:         Alejandro Pantoja-Zurita
+//  Deliverables:   Module #1, Lab #1
+//  Date:           1/22/2025
+//  Attributions:   this is the default code created by Visual Studio
+//                  I added comments and adjusted the output
+//  CSC114-Module1-Lab1.cpp : This file contains the 'main function. Program executions beings and ends there.
+//-------------------------------------------------------------------------
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "\n\n\n";
+    std::cout << "                <=================================>\n";
+    std::cout << "                 |                               |\n";
+    std::cout << "                 |    ! ! ! Hello World ! ! !    |\n";
+    std::cout << "                 |                               |\n";
+    std::cout << "                 |                               |\n";
+    std::cout << "                 |                               |\n";
+    std::cout << "                 |      My name is Alejandro     |\n";
+    std::cout << "                 |                               |\n";
+    std::cout << "                 |                               |\n";
+    std::cout << "                 |                               |\n";
+    std::cout << "                 |      ! ! ! Goodbye ! ! !      |\n";
+    std::cout << "                 |                               |\n";
+    std::cout << "                <================================>\n";
+}
